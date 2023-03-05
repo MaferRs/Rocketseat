f339a6f737beb43decb4f7679a9e64977d1817c2
Maria Fernanda <90789503+MaferRs@users.noreply.github.com>
2023-03-05 19:34:17 -0300

    continuação elementos dom

--- trilha-fundamentar/DOM/script.js
+++ trilha-fundamentar/DOM/script.js
@@ -25,7 +25,7 @@
 
 // elements.forEach(el => console.log(el))
 
-// Manipulando conteúdo
+// MANIPULANDO CONTEUDO
 
 //textContent
 
@@ -44,7 +44,7 @@
 // const element = document.querySelector('h1')
 // element.innerHTML = "Olá devs! <small> !!! </small>"
 
-//value
+//VALUE
 
 // const element = document.querySelector('input')
 
@@ -52,7 +52,7 @@
 // console.log(element.value)
 // element.value = 'valor que eu quiser'
 
-//atributos
+//ATRIBUTOS
 
 // const header = document.querySelector('header')
 // header.setAttribute('id', 'header')
@@ -66,19 +66,99 @@
 
 // header.setAttribute('class', 'bg header')
 
-// style
+// STYLE
 // const element = document.querySelector('body')
 // element.style.backgroundColor = '#f9f3d2'
 
 // console.log(element.style.backgroundColor)
 
-// classList
-const element = document.querySelector('body')
+// CLASSLIST
+// const element = document.querySelector('body')
 
-element.classList.add('active', 'green')
-console.log(element.classList)
-element.classList.remove('active')
-element.classList.toggle('active') // liga e desliga, adiciona e remove
+// element.classList.add('active', 'green')
+// console.log(element.classList)
+// element.classList.remove('active')
+// element.classList.toggle('active') // liga e desliga, adiciona e remove
 
 
+//parentNode parentElement
 
+// const body = document.querySelector('h1')
+// //console.log(body.parentNode)
+// console.log(body.parentElement)
+
+//childNodes  children
+
+// const el = document.querySelector('body')
+// console.log(el.childNodes)
+// console.log(el.children)
+//firstChild firstElementChild
+// console.log(el.firstChild)
+// console.log(el.firstElementChild)
+
+//lastChild lastElementChild
+
+// console.log(el.lastChild)
+// console.log(el.lastElementChild)
+
+//navegando pelo elementos
+// nextSibling nextElementSibling
+
+//navegando pelos elementos
+//nextSibling  nextElementSibling
+// previousSibling previousElementSibling
+
+// const el = document.querySelector('header')
+
+// console.log(el.nextSibling)
+// console.log(el.nextElementSibling)
+
+// console.log(el.previousSibling)
+// console.log(el.previousElementSibling)
+
+
+// criando e adicionando elementos
+
+// const div = document.createElement('div')
+// div.innerText = "olá devs"
+
+// const body = document.querySelector('body')
+
+// body.append(div)// adicionar algo depois
+// body.prepend(div)// adicionar algo antes
+
+
+// const div = document.createElement('div')
+// div.innerText = "fala mana"
+
+// // insertBefore
+// const body = document.querySelector('body')
+// const script = body.querySelector('script')
+// body.insertBefore(div, script)
+
+// body.insertBefore(div, header.nextSibling) // simula um insert after
+
+
+// EVENTOS
+
+// on - evento de click
+
+// function print() {
+//     console.log('clicou')
+// }
+
+// // eventos de teclado
+
+// const input = document.querySelector('input')
+
+// // input.onkeydown = function () {
+// //     console.log('rodei')
+// // }
+
+// // input.onkeyup = function () {
+// //     console.log('rodei')
+// // }
+
+// input.onkeypress = function () {
+//     console.log('rodei')
+// }
