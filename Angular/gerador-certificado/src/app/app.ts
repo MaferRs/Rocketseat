9dc1d8647000dd0118407851451bf70b561246da
Maria Fernanda <90789503+MaferRs@users.noreply.github.com>
2025-06-17 00:03:39 -0300

    ✨ list of certificates

--- Angular/gerador-certificado/src/app/app.ts
+++ Angular/gerador-certificado/src/app/app.ts
@@ -6,6 +6,7 @@ import { PrimaryButton } from './_components/primary-button/primary-button';
 import { SecondaryButton } from './_components/secondary-button/secondary-button';
 import { ItemCertificado } from './_components/item-certificado/item-certificado';
 import { BaseUi } from './_components/base-ui/base-ui';
+import { Certificados } from './pages/certificados/certificados';
 
 // CommonModule permite usar diretivar como ngIf, ngFor, mas preciso ser importado para funcionar.
 
@@ -18,6 +19,7 @@ import { BaseUi } from './_components/base-ui/base-ui';
     SecondaryButton,
     ItemCertificado,
     BaseUi,
+    Certificados,
   ],
   templateUrl: './app.html',
   styleUrl: './app.css',
